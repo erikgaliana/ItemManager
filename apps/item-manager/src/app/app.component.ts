c72b13c0cd8aa31303a7ad82cb752adae44ee009
Erik <55811560+erikgaliana@users.noreply.github.com>
2022-03-03 00:29:18 +0100

    Test get items (#4)
    * feature(get-items): create services barrel
    
    * refactor(get-items): delete nx-component
    
    * refactor(get-items): add loadItemsData
    
    * refactor(get-items): add store and effects module
    
    * refactor(get-items): delete getfakelist()

--- apps/item-manager/src/app/app.component.ts
+++ apps/item-manager/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ItemsFacade } from '@item-manager/items-services';
 
 @Component({
   selector: 'item-manager-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'item-manager';
+  constructor(private itemsFacade: ItemsFacade) {}
+
+  ngOnInit(): void {
+    this.itemsFacade.loadItemsData();
+  }
 }
