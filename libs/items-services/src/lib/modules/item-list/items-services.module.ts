0d4e7f888e414c0b255e0fdf6dd8562c39ae251b
erikgaliana <ekgaliana@gmail.com>
2022-03-02 23:40:37 +0100

    feature(add-ngrx): create items list services module

--- libs/items-services/src/lib/modules/item-list/items-services.module.ts
+++ libs/items-services/src/lib/modules/item-list/items-services.module.ts
@@ -1,20 +1,24 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+// Ngrx
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import * as fromItemsServices from './modules/item-list/items-services.reducer';
-import { ItemsServicesEffects } from './modules/item-list/items-services.effects';
-import { ItemsServicesFacade } from './modules/item-list/items-services.facade';
+
+// Store
+import { itemsReducers } from './store/reducers/reducer-map';
+import { ItemsFacade } from './facade/items-services.facade';
+import { ItemsEffects } from './store/effects/items-services.effects';
 
 @NgModule({
   imports: [
     CommonModule,
-    StoreModule.forFeature(
-      fromItemsServices.ITEMS_SERVICES_FEATURE_KEY,
-      fromItemsServices.reducer
-    ),
-    EffectsModule.forFeature([ItemsServicesEffects]),
+    HttpClientModule,
+    StoreModule.forFeature('items', itemsReducers),
+    EffectsModule.forFeature([ItemsEffects]),
   ],
-  providers: [ItemsServicesFacade],
+  providers: [ItemsFacade],
 })
-export class ItemsServicesModule {}
+export class ItemsStoreModule {}
