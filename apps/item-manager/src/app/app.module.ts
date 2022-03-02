29ba35f2a55c4b2a2ee4bd8cc09d5c699f86d52c
erikgaliana <ekgaliana@gmail.com>
2022-03-03 00:26:33 +0100

    refactor(get-items): add store and effects module

--- apps/item-manager/src/app/app.module.ts
+++ apps/item-manager/src/app/app.module.ts
@@ -1,13 +1,28 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+
+// NgRx
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+// Store
+import { ItemsFacade, ItemsStoreModule } from '@item-manager/items-services';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    ItemsStoreModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 30,
+    }),
+  ],
+  providers: [ItemsFacade],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
