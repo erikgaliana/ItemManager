ae7ef95c65a8a1a80509a186bce69f1338a9afc5
Erik <55811560+erikgaliana@users.noreply.github.com>
2022-03-12 12:33:10 +0100

    Release/0.2.0 (#7)
    * feature(servicesScaffolding) create item-services (#1)
    
    * Feature/add ngrx (#3)
    
    * feature(add-ngrx) install ngrx
    
    * feature(add-ngrx): organize folders
    
    * feature(add-ngrx): create actions
    
    * feature(add-ngrx): create models
    
    * feature(add-ngrx): create items api
    
    * feature(add-ngrx): create effects
    
    * feature(add-ngrx): create reducers
    
    * feature(add-ngrx): create selectors
    
    * feature(add-ngrx): create facade
    
    * feature(add-ngrx): create items list services module
    
    * refactor(add-ngrx): correct typo
    
    * Test get items (#4)
    
    * feature(get-items): create services barrel
    
    * refactor(get-items): delete nx-component
    
    * refactor(get-items): add loadItemsData
    
    * refactor(get-items): add store and effects module
    
    * refactor(get-items): delete getfakelist()
    
    * feature(add-ionic):install ionic (#6)

---
+++ apps/ion-item-manager/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ion-item-manager',
+  webDir: '../../dist/apps/ion-item-manager',
+  bundledWebRuntime: false,
+};
+
+export default config;
