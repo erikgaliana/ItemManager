e64cc690a8567954183111064277dd6017f44fa3
erikgaliana <ekgaliana@gmail.com>
2022-03-12 18:10:55 +0100

    feature(add-item-list): redirect to public

--- apps/ion-item-manager/src/app/app-routing.module.ts
+++ apps/ion-item-manager/src/app/app-routing.module.ts
@@ -3,14 +3,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () =>
-      import('./home/home.module').then((m) => m.HomePageModule),
+    path: '',
+    redirectTo: 'public',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'public',
+    loadChildren: () =>
+      import('./public/public.module').then((m) => m.PublicModule),
+    data: { preload: true },
   },
 ];
 
