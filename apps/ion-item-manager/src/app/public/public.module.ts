6a46e80f0366f2d06da23d06fcccf8fc7080dc95
erikgaliana <ekgaliana@gmail.com>
2022-03-14 00:01:31 +0100

    feature(add-item-list): feature add items facade

--- apps/ion-item-manager/src/app/public/public.module.ts
+++ apps/ion-item-manager/src/app/public/public.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { ItemsFacade } from '@item-manager/items-services';
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicViewComponent } from './public.component';
 import { ShellComponentModule } from './shell/shell.module';
@@ -7,5 +8,6 @@ import { ShellComponentModule } from './shell/shell.module';
 @NgModule({
   declarations: [PublicViewComponent],
   imports: [PublicRoutingModule, IonicModule, ShellComponentModule],
+  providers: [ItemsFacade],
 })
 export class PublicModule {}
