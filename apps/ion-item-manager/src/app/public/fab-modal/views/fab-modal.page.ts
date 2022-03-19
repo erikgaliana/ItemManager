daa5ed6d8b47b3e040c4a1198d5d7ba45e556779
erikgaliana <ekgaliana@gmail.com>
2022-03-19 01:11:52 +0100

    feature(filtered-list): add modal page

--- apps/ion-item-manager/src/app/public/fab-modal/views/fab-modal.page.ts
+++ apps/ion-item-manager/src/app/public/fab-modal/views/fab-modal.page.ts
@@ -4,6 +4,13 @@ import {
   Component,
   ViewEncapsulation,
 } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+// Ionic
+import { ModalController } from '@ionic/angular';
+
+// Store
+import { ItemsFacade } from '@item-manager/items-services';
 
 @Component({
   selector: 'item-manager-fab-modal',
@@ -12,4 +19,26 @@ import {
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FabModalComponent {}
+export class FabModalComponent {
+  constructor(
+    public itemsFacade: ItemsFacade,
+    private modalCtrl: ModalController,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  trackByItems(index: number, item: any): number {
+    return item.id;
+  }
+
+  deleteFab(i: number): void {
+    this.itemsFacade.deleteFab(i);
+  }
+
+  close() {
+    this.modalCtrl.dismiss();
+  }
+
+  getImgContent(image: string): SafeUrl {
+    return this.sanitizer.bypassSecurityTrustUrl(image);
+  }
+}
